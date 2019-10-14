41dc9fc9bdc21b257f3175053b6d5db6b8aecd8b
thibaut <tgorioux@ekylibre.com>
2019-10-14 18:28:07 +0200

    refacto following review code remarks: clean map.js, map base helper and controller cadastral_land_parcel_zones

--- app/controllers/backend/cadastral_land_parcel_zones_controller.rb
+++ app/controllers/backend/cadastral_land_parcel_zones_controller.rb
@@ -3,8 +3,6 @@ module Backend
     respond_to :json
 
     def index
-      selected_ids = params[:selected_ids] ? params[:selected_ids].split(',') : []
-
       records_in_bounding_box = CadastralLandParcelZone.in_bounding_box(params[:bounding_box])
 
       records_not_in_cvi = records_in_bounding_box.joins(<<~SQL).where('land_parcel_id IS NULL')
@@ -15,8 +13,8 @@ module Backend
       records = records_not_in_cvi.collect do |r|
         [r[:shape], { uuid: r.id, cadastral_ref: r.id[/\D{1,2}\d*/].tr('0', '') }]
       end.transpose
-      crop_zones = Charta.new_geometry "GEOMETRYCOLLECTION(#{Maybe(records.first).join(',').or_else { '' }})"
-      respond_with crop_zones.to_json_feature_collection(records.last || [])
+      land_parcel_zones = Charta.new_geometry "GEOMETRYCOLLECTION(#{Maybe(records.first).join(',').or_else { '' }})"
+      respond_with land_parcel_zones.to_json_feature_collection(records.last || [])
     end
   end
 end
--- app/helpers/map/base_helper.rb
+++ app/helpers/map/base_helper.rb
@@ -2,109 +2,6 @@
 
 module Map
   module BaseHelper
-    def resource
-      return send(controller_name.singularize) if respond_to?(controller_name.singularize)
-
-      instance_variable_get('@' + controller_name.singularize)
-    end
-
-    def resource_model
-      controller_name.classify.constantize
-    end
-
-    def user_preference_value(name, default = nil)
-      preference = current_user.preferences.find_by(name: name)
-      preference ? preference.value : default
-    end
-
-    def collection
-      instance_variable_get('@' + controller_name)
-    end
-
-    def drawer(*_args, &block)
-      # ???: what are *args used for ?
-      content_for(:drawer) do
-        content_tag(:div, class: 'drawer', id: 'drawer', &block)
-      end
-    end
-
-    def drawer_header(text)
-      content_tag(:div, class: 'drawer-header') do
-        # content_tag(:a, class: 'close', data: { dismiss: 'drawer' }) do
-        #   content_tag(:span, '×')
-        # end +
-        content_tag(:h4, text, class: 'drawer-title')
-      end
-    end
-
-    def active_url?(url_options)
-      controller_path == url_options[:controller].to_s &&
-        action_name == url_options[:action].to_s
-    end
-
-    def link_or_not_to(text, url_options = {}, html_options = {})
-      url_options[:controller] ||= controller_path
-      url_options[:action] ||= :index
-      if active_url?(url_options)
-        html_options[:disabled] = true
-        html_options[:class] ||= ''
-        html_options[:class] << ' active'
-      end
-      link_to(text, url_options, html_options)
-    end
-
-    # Build a JSON for a data-tour parameter and put it on <body> element
-    def tour(name, _options = {})
-      preference = current_user.preference("interface.tours.#{name}.finished", false, :boolean)
-      return if preference.value
-
-      object = {}
-      object[:defaults] ||= {}
-      object[:defaults][:classes] ||= 'shepherd-theme-arrows'
-      object[:defaults][:show_cancel_link] = true unless object[:defaults].key?(:show_cancel_link)
-      unless object[:defaults][:buttons]
-        buttons = []
-        buttons << {
-          text: :next.tl,
-          classes: 'btn btn-primary',
-          action: 'next'
-        }
-        object[:defaults][:buttons] = buttons
-      end
-      lister = Ekylibre::Support::Lister.new(:step)
-      yield lister
-      return nil unless lister.any?
-
-      steps = lister.steps.map do |step|
-        id = step.args.first
-        on = (step.options[:on] || 'center').to_s
-        if reading_ltr?
-          if on =~ /right/
-            on.gsub!('right', 'left')
-          else
-            on.gsub!('left', 'right')
-          end
-        end
-        attributes = {
-          id: id,
-          title: "tours.#{name}.#{id}.title".tl,
-          text: "tours.#{name}.#{id}.content".tl,
-          attachTo: {
-            element: step.options[:element] || '#' + id.to_s,
-            on: on.tr('_', ' ')
-          }
-        }
-        if step == lister.steps.last
-          attributes[:buttons] = [{ text: :finished.tl, classes: 'btn btn-primary', action: 'next' }]
-        end
-        attributes
-      end
-      object[:name] = name
-      object[:url] = finish_backend_tour_path(id: name)
-      object[:steps] = steps
-      content_for(:tour, object.jsonize_keys.to_json)
-    end
-
     def map_config(options = {})
       {
         box: {
@@ -122,13 +19,6 @@ module Map
         ], defaultCenter: {
           lat: 46.74738913515841,
           lng: 2.493896484375
-        },
-        cut: {
-          cycling: 12,
-          panel: {
-            animatedHelper: image_url('animations/cut.gif'),
-            subtitleProperty: :set_timeline_to_correct_date.tl
-          }
         }
       }.merge options
     end
