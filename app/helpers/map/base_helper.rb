4caac88840d93d9d726ae4cccf1b41f9cd57f221
thibaut <tgorioux@ekylibre.com>
2019-10-22 10:54:25 +0200

    backgrounds list extracted into method helper

--- app/helpers/map/base_helper.rb
+++ app/helpers/map/base_helper.rb
@@ -8,15 +8,18 @@ module Map
           width: '100%',
           height: '100%'
         },
-        backgrounds:
-          MapLayer.available_backgrounds.map { |e| [e.reference_name&.split('.')&.map(&:camelize)&.join('.'), e.to_json_object] }.unshift(
-            ['MapBox', { id: 'mapbox.satellite', accessToken: ENV['MAPBOX_API_KEY'], maxZoom: 23 }]
-          ),
+        backgrounds: backgrounds,
         defaultCenter: {
           lat: 46.74738913515841,
           lng: 2.493896484375
         }
       }.merge options
+    end
+
+    def backgrounds
+      MapLayer.available_backgrounds.map { |e| [e.reference_name&.split('.')&.map(&:camelize)&.join('.'), e.to_json_object] }.unshift(
+        ['MapBox', { id: 'mapbox.satellite', accessToken: ENV['MAPBOX_API_KEY'], maxZoom: 23 }]
+      )
     end
   end
 end
