e10c31695051816242b35087e9f42d30c07f73e5
thibaut <tgorioux@ekylibre.com>
2019-11-25 11:37:46 +0100

    cvi exchanger work with fixtures

--- test/exchangers/ekylibre/cvi_exchanger_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Ekylibre
-  class CviExchangerTest < ActiveExchanger::TestCase
-    test 'import' do
-      Ekylibre::CviExchanger.import(fixture_files_path.join('imports', 'ekylibre', 'cvi.csv'))
-    end
-  end
-end
