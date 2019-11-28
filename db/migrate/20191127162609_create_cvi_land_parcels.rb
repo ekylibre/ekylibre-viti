5cb60a5b4a7f022c256f8f739685e34c78274c70
thibaut <tgorioux@ekylibre.com>
2019-11-28 11:38:11 +0100

    add constraints on cvi_land_parcel table and clean:schema

--- db/migrate/20191127162609_create_cvi_land_parcels.rb
+++ db/migrate/20191127162609_create_cvi_land_parcels.rb
@@ -1,21 +1,21 @@
 class CreateCviLandParcels < ActiveRecord::Migration
   def change
     create_table :cvi_land_parcels do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :commune
       t.string :locality
       t.integer :designation_of_origin_id
       t.string :vine_variety_id
       t.string :calculated_area_unit
-      t.decimal :calculated_area_value
+      t.decimal :calculated_area_value, precision: 19, scale: 4
       t.string :declared_area_unit
-      t.decimal :declared_area_value
-      t.geometry :shape
+      t.decimal :declared_area_value, precision: 19, scale: 4
+      t.geometry :shape, srid: 4326
       t.references :campaign, index: true, foreign_key: true
       t.string :rootstock_id
-      t.decimal :inter_vine_plant_distance_value
+      t.decimal :inter_vine_plant_distance_value, precision: 19, scale: 4
       t.string :inter_vine_plant_distance_unit
-      t.decimal :inter_row_distance_value
+      t.decimal :inter_row_distance_value, precision: 19, scale: 4
       t.string :inter_row_distance_unit
       t.string :state
       t.references :cvi_cultivable_zone, index: true, foreign_key: true
