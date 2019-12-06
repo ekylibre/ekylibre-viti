8be4a313ee13bdf6c78c909382c958462fe3f174
Paul Quintrie Lamothe <paul.quintrie.lamothe@hotmail.fr>
2019-12-06 10:59:37 +0100

    add tests in journal entries exchanger test to be sure the import has been correctly done

--- test/exchangers/sage/i_seven/journal_entries_exchanger_test.rb
+++ test/exchangers/sage/i_seven/journal_entries_exchanger_test.rb
@@ -4,7 +4,19 @@ module Sage
   module ISeven
     class JournalEntriesExchangerTest < ActiveExchanger::TestCase
       test 'import' do
+        FinancialYear.delete_all
         Sage::ISeven::JournalEntriesExchanger.import(fixture_files_path.join('imports', 'sage', 'i_seven', 'journal_entries.ecx'))
+
+        journal = Journal.find_by(name: "Ventes eaux-de-vie")
+        journal_entry_1 = journal.entries.find_by(number: 1)
+        item1 = journal_entry_1.items
+
+        assert_equal 2, journal.entries.count
+        assert_equal 4, journal_entry_1.items.count
+        assert_equal 4364.8, item1.find_by(account_id: 693).real_credit.to_f
+        assert_equal 4363.49, item1.find_by(account_id: 688).real_debit.to_f
+        assert_equal 1.06, item1.find_by(account_id: 691).real_debit.to_f
+        assert_equal 0.25, item1.find_by(account_id: 692).real_debit.to_f
       end
     end
   end
