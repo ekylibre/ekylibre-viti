6b78b872d5545d3ecd213f1f193e2e93d2f78c57
thibaut <tgorioux@ekylibre.com>
2019-12-24 11:50:36 +0100

    refacto model tests

--- test/models/cvi_cultivable_zone_test.rb
+++ test/models/cvi_cultivable_zone_test.rb
@@ -1,33 +1,28 @@
 require 'test_helper'
 
 class CviCultivableZoneTest < Ekylibre::Testing::ApplicationTestCase::WithFixtures
-  should 'be creatable' do
+  it 'is creatable' do
     resource = create(:cvi_cultivable_zone)
     first_resource = CviCultivableZone.last
     assert_equal resource, first_resource
   end
 
-  context 'Aggregations' do
-    should 'respond to calculated_area  with measure object' do
-      resource = create(:cvi_cultivable_zone)
-      assert_equal 'Measure', resource.calculated_area.class.name
-    end
-    should 'respond to declared_area  with measure object' do
-      resource = create(:cvi_cultivable_zone)
-      assert_equal 'Measure', resource.declared_area.class.name
-    end
+  it 'responds to calculated_area  with measure object' do
+    resource = create(:cvi_cultivable_zone)
+    assert_equal 'Measure', resource.calculated_area.class.name
   end
 
-  context 'validations' do
-    should validate_presence_of(:name)
+  it 'responds to declared_area  with measure object' do
+    resource = create(:cvi_cultivable_zone)
+    assert_equal 'Measure', resource.declared_area.class.name
   end
 
-  context 'associations' do
-    should belong_to(:cvi_statement)
-    should have_many(:cvi_cadastral_plants)
-    should have_many(:cvi_land_parcels)
-    should have_many(:locations)
-  end
+  should validate_presence_of(:name)
+
+  should belong_to(:cvi_statement)
+  should have_many(:cvi_cadastral_plants)
+  should have_many(:cvi_land_parcels)
+  should have_many(:locations)
 
   should enumerize(:land_parcels_status).in(:not_created,:created).with_predicates(true)
 end
--- test/models/cvi_land_parcel_test.rb
+++ test/models/cvi_land_parcel_test.rb
@@ -1,59 +1,49 @@
 require 'test_helper'
 
 class CviLandParcelTest < Ekylibre::Testing::ApplicationTestCase::WithFixtures
-  should 'be creatable' do
+  it 'is creatable' do
     cvi_land_parcel = create(:cvi_land_parcel)
     first_cvi_land_parcel = CviLandParcel.last
     assert_equal cvi_land_parcel, first_cvi_land_parcel
   end
 
-  context 'Aggregations' do
-    should 'respond to calculated_area  with measure object' do
-      resource = create(:cvi_land_parcel)
-      assert_equal 'Measure', resource.calculated_area.class.name
-    end
-    should 'respond to declared_area  with measure object' do
-      resource = create(:cvi_land_parcel)
-      assert_equal 'Measure', resource.declared_area.class.name
-    end
-    should 'respond to inter_row_distance  with measure object' do
-      resource = create(:cvi_land_parcel)
-      assert_equal 'Measure', resource.inter_row_distance.class.name
-    end
-    should 'respond to inter_vine_plant_distance with measure object' do
-      resource = create(:cvi_land_parcel)
-      assert_equal 'Measure', resource.inter_vine_plant_distance.class.name
-    end
+  it 'responds to calculated_area with measure object' do
+    resource = create(:cvi_land_parcel)
+    assert_equal 'Measure', resource.calculated_area.class.name
+  end
+  it 'responds to declared_area with measure object' do
+    resource = create(:cvi_land_parcel)
+    assert_equal 'Measure', resource.declared_area.class.name
+  end
+  it 'responds to inter_row_distance with measure object' do
+    resource = create(:cvi_land_parcel)
+    assert_equal 'Measure', resource.inter_row_distance.class.name
+  end
+  it 'responds to inter_vine_plant_distance with measure object' do
+    resource = create(:cvi_land_parcel)
+    assert_equal 'Measure', resource.inter_vine_plant_distance.class.name
   end
 
-  context 'validations' do
-    should validate_presence_of(:name)
-  end
+  should validate_presence_of(:name)
 
-  context 'associations' do
-    should belong_to(:cvi_cultivable_zone)
-    should belong_to(:designation_of_origin).with_foreign_key('designation_of_origin_id')
-    should belong_to(:vine_variety).with_foreign_key('vine_variety_id')
-    should have_many(:land_parcel_rootstocks)
-    should have_many(:locations)
-  end
+  should belong_to(:cvi_cultivable_zone)
+  should belong_to(:designation_of_origin).with_foreign_key('designation_of_origin_id')
+  should belong_to(:vine_variety).with_foreign_key('vine_variety_id')
+  should have_many(:land_parcel_rootstocks)
+  should have_many(:locations)
 
   should enumerize(:state).in(:planted, :removed_with_authorization).with_predicates(true)
 
-  describe '#updated?' do
-    it 'return true if resource have already been updated' do
-      resource = create(:cvi_land_parcel)
-      resource.update(attributes_for(:cvi_land_parcel))
-      assert(resource.reload.updated?)
-    end
+  it 'responds to updated? with true, if it has already been updated' do
+    resource = create(:cvi_land_parcel)
+    resource.update(attributes_for(:cvi_land_parcel))
+    assert(resource.reload.updated?)
   end
 
-  describe 'callbacks' do
-    it 'calculated_area is updated when shape change' do
-      resource = create(:cvi_land_parcel)
-      shape = FFaker::Shape.multipolygon
-      resource.update(shape: shape)
-      assert_equal Measure.new(shape.area, :square_meter).convert(:hectare).value.round(5), resource.reload.calculated_area_value
-    end
+  it 'has calculated_area updated when shape change' do
+    resource = create(:cvi_land_parcel)
+    shape = FFaker::Shape.multipolygon
+    resource.update(shape: shape)
+    assert_equal Measure.new(shape.area, :square_meter).convert(:hectare).value.round(5), resource.reload.calculated_area_value
   end
 end
