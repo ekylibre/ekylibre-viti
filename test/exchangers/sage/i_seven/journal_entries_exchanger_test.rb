5cc18c8dee5de983f60b579de6522849b3e873ac
Paul Quintrie Lamothe <paul.quintrie.lamothe@hotmail.fr>
2020-01-03 14:44:14 +0100

    refacto test regarding some comments

--- test/exchangers/sage/i_seven/journal_entries_exchanger_test.rb
+++ test/exchangers/sage/i_seven/journal_entries_exchanger_test.rb
@@ -18,15 +18,15 @@ module Sage
 
         assert_equal 2, journal1.entries.count
         assert_equal 1, journal2.entries.count
-        assert_equal 4364.8, item1.where(account_id: Account.where(number: '707100000').pluck(:id)).first.real_credit.to_f
-        assert_equal 4363.49, item1.where(account_id: Account.where(number: '411008162').pluck(:id)).first.real_debit.to_f
-        assert_equal 1.06, item1.where(account_id: Account.where(number: '637800000').pluck(:id)).first.real_debit.to_f
-        assert_equal 0.25, item1.where(account_id: Account.where(number: '637801000').pluck(:id)).first.real_debit.to_f
+        assert_equal 4364.8, item1.where(account: Account.where(number: '707100000')).first.real_credit.to_f
+        assert_equal 4363.49, item1.where(account: Account.where(number: '411008162')).first.real_debit.to_f
+        assert_equal 1.06, item1.where(account: Account.where(number: '637800000')).first.real_debit.to_f
+        assert_equal 0.25, item1.where(account: Account.where(number: '637801000')).first.real_debit.to_f
 
         assert_equal 4, journal_entry_1.items.count
         assert_equal 2, journal_entry_2.items.count
-        assert_equal 282.18, item2.where(account_id: Account.where(number: '401000483').pluck(:id)).first.real_debit.to_f
-        assert_equal 282.18, item2.where(account_id: Account.where(number: '512300000').pluck(:id)).first.real_credit.to_f
+        assert_equal 282.18, item2.where(account: Account.where(number: '401000483')).first.real_debit.to_f
+        assert_equal 282.18, item2.where(account: Account.where(number: '512300000')).first.real_credit.to_f
         assert_equal '512300000', cash_main_account
         assert Account.find_by(number: '401000483')
       end
