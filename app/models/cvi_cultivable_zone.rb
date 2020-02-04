34351baad0c9494e033b7dad1100cde1718a5fec
thibaut <tgorioux@ekylibre.com>
2020-02-04 13:44:20 +0100

    add shaped module to cvi_cultivable_zone and cvi_land_parcel

--- app/models/cvi_cultivable_zone.rb
+++ app/models/cvi_cultivable_zone.rb
@@ -1,4 +1,6 @@
 class CviCultivableZone < Ekylibre::Record::Base
+  include Shaped
+
   composed_of :calculated_area, class_name: 'Measure', mapping: [%w[calculated_area_value to_d], %w[calculated_area_unit unit]]
   composed_of :declared_area, class_name: 'Measure', mapping: [%w[declared_area_value to_d], %w[declared_area_unit unit]]
 
@@ -9,19 +11,9 @@ class CviCultivableZone < Ekylibre::Record::Base
 
   validates_presence_of :name
 
-  before_save :set_calculated_area, on: %i[create update], if: :shape_changed?
-
   enumerize :land_parcels_status, in: %i[not_created created], predicates: true
-
-  def shape
-    Charta.new_geometry(self[:shape])
-  end
 
   def has_cvi_land_parcels?
     cvi_land_parcels.any?
-  end
-
-  def set_calculated_area
-    self.calculated_area = Measure.new(shape.area, :square_meter).convert(:hectare)
   end
 end
