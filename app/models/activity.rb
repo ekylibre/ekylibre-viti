6dfedff02867cd3fdd965a15def9b3eeb2b61aa2
thibaut <tgorioux@ekylibre.com>
2020-04-02 10:55:00 +0200

    use nomenclature variety instead of production nature to scope vine activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -115,7 +115,7 @@ class Activity < Ekylibre::Record::Base
   scope :actives, -> { availables.where(id: ActivityProduction.where(state: :opened).select(:activity_id)) }
   scope :availables, -> { where.not('suspended') }
   scope :main, -> { where(nature: 'main') }
-  scope :wine, -> { joins(:production_nature).where( "human_name_fra LIKE 'Vigne%'") }
+  scope :wine, -> { where(cultivation_variety: Nomen::Variety.find('vitis').children.map(&:name)) }
 
   scope :of_campaign, lambda { |campaign|
     if campaign
