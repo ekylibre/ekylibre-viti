ccc5fa1b1b3cbd90e34bc694aa4be052710645d8
thibaut <tgorioux@ekylibre.com>
2020-04-08 11:58:38 +0200

    change modal body message and button label

--- db/structure.sql
+++ db/structure.sql
@@ -503,8 +503,8 @@ CREATE TABLE public.intervention_parameters (
     variety character varying,
     batch_number character varying,
     usage_id character varying,
-    allowed_entry_factor integer,
-    allowed_harvest_factor integer
+    allowed_entry_factor interval,
+    allowed_harvest_factor interval
 );
 
 
@@ -3552,7 +3552,6 @@ SELECT
     NULL::integer AS inter_vine_plant_distance_value,
     NULL::integer AS inter_row_distance_value,
     NULL::character varying AS state,
-    NULL::character varying AS activity_name,
     NULL::integer AS cvi_cultivable_zone_id;
 
 
@@ -18533,19 +18532,14 @@ CREATE OR REPLACE VIEW public.formatted_cvi_land_parcels AS
     (cvi_land_parcels.inter_vine_plant_distance_value)::integer AS inter_vine_plant_distance_value,
     (cvi_land_parcels.inter_row_distance_value)::integer AS inter_row_distance_value,
     cvi_land_parcels.state,
-        CASE
-            WHEN (activities.name IS NULL) THEN 'A définir'::character varying
-            ELSE activities.name
-        END AS activity_name,
     cvi_land_parcels.cvi_cultivable_zone_id
-   FROM ((((((public.cvi_land_parcels
+   FROM (((((public.cvi_land_parcels
      LEFT JOIN public.locations locations ON (((cvi_land_parcels.id = locations.localizable_id) AND ((locations.localizable_type)::text = 'CviLandParcel'::text))))
-     LEFT JOIN public.activities ON ((cvi_land_parcels.activity_id = activities.id)))
      LEFT JOIN ___lexicon.master_vine_varieties rootstocks ON (((cvi_land_parcels.rootstock_id)::text = (rootstocks.id)::text)))
      LEFT JOIN ___lexicon.registered_postal_zones ON (((locations.registered_postal_zone_id)::text = (registered_postal_zones.id)::text)))
      LEFT JOIN ___lexicon.master_vine_varieties vine_varieties ON (((cvi_land_parcels.vine_variety_id)::text = (vine_varieties.id)::text)))
      LEFT JOIN ___lexicon.registered_protected_designation_of_origins designation_of_origins ON ((cvi_land_parcels.designation_of_origin_id = designation_of_origins.id)))
-  GROUP BY cvi_land_parcels.id, designation_of_origins.product_human_name_fra, vine_varieties.specie_name, (initcap((rootstocks.specie_name)::text)), activities.name;
+  GROUP BY cvi_land_parcels.id, designation_of_origins.product_human_name_fra, vine_varieties.specie_name, (initcap((rootstocks.specie_name)::text));
 
 
 --
@@ -19884,6 +19878,8 @@ INSERT INTO schema_migrations (version) VALUES ('20200128133347');
 INSERT INTO schema_migrations (version) VALUES ('20200213102154');
 
 INSERT INTO schema_migrations (version) VALUES ('20200225093814');
+
+INSERT INTO schema_migrations (version) VALUES ('20200312163243');
 
 INSERT INTO schema_migrations (version) VALUES ('20200313161422');
 
