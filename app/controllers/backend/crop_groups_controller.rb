97b957d3759c5f6d8288c6170c4661adee758e50
thibaut <tgorioux@ekylibre.com>
2020-04-30 18:47:42 +0200

    update index view

--- app/controllers/backend/crop_groups_controller.rb
+++ app/controllers/backend/crop_groups_controller.rb
@@ -1,8 +1,12 @@
 class Backend::CropGroupsController < Backend::BaseController
   manage_restfully only: %i[index new edit create update destroy]
 
-  list selectable: true do |t|
+  list selectable: true, join: :crop_group_items do |t|
     t.action :edit
     t.action :destroy, if: :destroyable?
+    t.column :name
+    t.column :uses, label_method: :label_names, label: :usages 
+    t.column :crop_names, label_method: :crop_names, label: :land_parcels_plants
+    t.column :total_area, label_method: :total_area, label: :total_area
   end
 end
--- app/models/crop_group.rb
+++ app/models/crop_group.rb
@@ -3,8 +3,6 @@ class CropGroup < Ekylibre::Record::Base
 
   has_many :labellings, class_name: 'CropGroupLabelling', dependent: :destroy
   has_many :items, class_name: 'CropGroupItem', dependent: :nullify 
-    has_many :items, class_name: 'CropGroupItem', dependent: :nullify 
-  has_many :items, class_name: 'CropGroupItem', dependent: :nullify 
   has_many :plants, through: :items, source: :crop, source_type: 'Plant'
   has_many :land_parcels, through: :items, source: :crop, source_type: 'LandParcel'
   has_many :labels, through: :labellings
@@ -14,5 +12,19 @@ class CropGroup < Ekylibre::Record::Base
   accepts_nested_attributes_for :labellings, allow_destroy: true
   accepts_nested_attributes_for :items, allow_destroy: true
 
-    validates :name, presence: true
+  def crops 
+    plants + land_parcels
+  end
+
+  def label_names
+    labels.collect(&:name).join('')
+  end
+
+  def crop_names
+    crops.collect(&:name).join('')
+  end
+
+  def total_area
+    crops.collect(&:net_surface_area).sum
+  end
 end
