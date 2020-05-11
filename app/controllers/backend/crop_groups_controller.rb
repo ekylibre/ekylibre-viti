9242955283fdb50fba9708b41998aa9a1243e543
thibaut <tgorioux@ekylibre.com>
2020-05-11 09:59:13 +0200

    add search on land parcel plant name

--- app/controllers/backend/crop_groups_controller.rb
+++ app/controllers/backend/crop_groups_controller.rb
@@ -3,7 +3,7 @@ class Backend::CropGroupsController < Backend::BaseController
 
   def self.crop_groups_conditions
     code = ''
-    code << search_conditions(crop_groups: %i[name target], labels: %i[name]) + " ||= []\n"
+    code << search_conditions(crop_groups: %i[name target], labels: %i[name], products: %i[name]) + " ||= []\n"
     code << "unless params[:label].blank? \n"
     code << "  c[0] << ' AND labels.name = ?'\n"
     code << "  c << params[:label]\n"
@@ -12,13 +12,15 @@ class Backend::CropGroupsController < Backend::BaseController
     code.c
   end
 
-  list selectable: true, conditions: crop_groups_conditions, joins:"LEFT JOIN crop_group_labellings ON crop_group_labellings.crop_group_id = crop_groups.id 
-  LEFT JOIN labels ON labels.id = crop_group_labellings.label_id" do |t|
+  list selectable: true, conditions: crop_groups_conditions, joins: "LEFT JOIN crop_group_labellings ON crop_group_labellings.crop_group_id = crop_groups.id
+    LEFT JOIN labels ON labels.id = crop_group_labellings.label_id
+    LEFT JOIN crop_group_items ON crop_group_items.crop_group_id = crop_groups.id
+    LEFT JOIN products ON products.id = crop_group_items.crop_id ", distinct: true do |t|
     t.action :edit
     t.action :destroy, if: :destroyable?
     t.action :duplicate, method: :post
     t.column :name, url: true
-    t.column :uses, label_method: :label_names, label: :usages 
+    t.column :uses, label_method: :label_names, label: :use 
     t.column :crop_names, label_method: :crop_names, label: :land_parcels_plants
     t.column :total_area, label_method: :total_area, label: :total_area
   end
