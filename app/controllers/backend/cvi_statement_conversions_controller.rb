8c160a5b43831670567704a1713acced35c982f0
thibaut <tgorioux@ekylibre.com>
2020-06-10 11:04:38 +0200

    show cvi_land_parcels calculated area in list on  cvi_cultivable_zone show view"

--- app/controllers/backend/cvi_statement_conversions_controller.rb
+++ app/controllers/backend/cvi_statement_conversions_controller.rb
@@ -10,8 +10,9 @@ module Backend
       t.column :name
       t.column :communes, label: :communes
       t.column :cadastral_references, label: :cadastral_references
-      t.column :formatted_declared_area, label: :declared_area
-      t.column :formatted_calculated_area, label: :calculated_area
+      t.column :formatted_calculated_area, label: :surface
+      t.column :formatted_declared_area, label: :cvi_land_parcels_declared_area
+      t.column :cvi_land_parcels_calculated_area, label: :cvi_land_parcels_calculated_area
       t.column :land_parcels_status
       t.action :generate_cvi_land_parcels, unless: :has_cvi_land_parcels?, url: { controller: 'cvi_cultivable_zones' }
       t.action :edit_cvi_land_parcels, if: :has_cvi_land_parcels?, url: { controller: 'cvi_cultivable_zones' }
