1fae9af169bcf63f25e05a0a9bae2402f43ff7b1
Thibaut Gorioux <tgorioux@ekylibre.com>
2020-08-27 11:40:13 +0200

    Use label for error message in exchanger

--- app/exchangers/ekylibre/cvi_csv_exchanger.rb
+++ app/exchangers/ekylibre/cvi_csv_exchanger.rb
@@ -4,16 +4,16 @@ module Ekylibre
 
     def check
       file_extension = File.extname(file)
-      raise ::I18n.translate('exchangers.ekylibre_cvi.errors.wrong_file_extension', file_extension: file_extension, required_file_extension: '.csv') if file_extension != '.csv'
+      raise :wrong_file_extension.tl(file_extension: file_extension, required_file_extension: '.csv') if file_extension != '.csv'
 
-      CSV.foreach(file, headers: true, header_converters: ->(h) { HEADER_CONVERSION[h] || (raise ::I18n.translate('exchangers.ekylibre_cvi.errors.unknown_column_name', column_name: h)) }) do |row|
-        raise ::I18n.translate('exchangers.ekylibre_cvi.errors.unknown_state', state: row[:state]) unless STATES[row[:state]]
-        raise ::I18n.translate('exchangers.ekylibre_cvi.errors.invalid_siret', value: row[:siret_number]) unless Luhn.valid?(row[:siret_number])
+      CSV.foreach(file, headers: true, header_converters: ->(h) { HEADER_CONVERSION[h] || (raise :unknown_column_name.tl(column_name: h)) }) do |row|
+        raise :unknown_state.tl(state: row[:state]) unless STATES[row[:state]]
+        raise :invalid_siret.tl(value: row[:siret_number]) unless Luhn.valid?(row[:siret_number])
       end
 
       @cvi_row_list = CSV.new(File.open(file).read, headers: true, header_converters: ->(h) { HEADER_CONVERSION[h] }).to_a.map(&:to_hash)
 
-      raise ::I18n.translate('exchangers.ekylibre_cvi.errors.cvi_statement_already_exist') if cvi_statement_already_exist?
+      raise :cvi_statement_already_exist.tl if cvi_statement_already_exist?
 
       true
     end
