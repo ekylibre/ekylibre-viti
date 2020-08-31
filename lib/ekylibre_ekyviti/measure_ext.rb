f5b32ee70d2053ea5ae70196350870b4c201ee6d
Thibaut Gorioux <tgorioux@ekylibre.com>
2020-08-31 10:53:48 +0200

    Drop when new nomencalture is used

--- lib/ekylibre_ekyviti/measure_ext.rb
+++ lib/ekylibre_ekyviti/measure_ext.rb
@@ -6,8 +6,8 @@ module EkylibreEkyviti
         area.in(:hectare)
         total_area_to_s = (area.value * 10_000).to_f.floor.to_s.rjust(6, '0')
         [[total_area_to_s[0..-5], Nomen::Unit[:hectare].symbol],
-        [total_area_to_s[-4, 2], Nomen::Unit[:are].symbol],
-        [total_area_to_s[-2, 2], Nomen::Unit[:centiare].symbol]].map(&:join).join(' ')
+         [total_area_to_s[-4, 2], Nomen::Unit[:are].symbol],
+         [total_area_to_s[-2, 2], "ca"]].map(&:join).join(' ')
       },
       short_form_unit: lambda { |measure| "#{measure.value.to_f} #{measure.symbol}" }
     }.freeze
