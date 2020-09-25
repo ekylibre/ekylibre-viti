41314a461a6844414da15ffed5a30f07cf85e957
Thibaut Gorioux <tgorioux@ekylibre.com>
2020-09-25 11:15:50 +0200

    Use Onoma instead of Noemen

--- lib/ekylibre_ekyviti/measure_ext.rb
+++ lib/ekylibre_ekyviti/measure_ext.rb
@@ -5,8 +5,8 @@ module EkylibreEkyviti
         # format area: 1,0056 ha => 01ha 56ca,  1,3456 ha => 01ha 34ar 56ca
         area.in(:hectare)
         total_area_to_s = (area.value * 10_000).to_f.floor.to_s.rjust(6, '0')
-        [[total_area_to_s[0..-5], Nomen::Unit[:hectare].symbol],
-         [total_area_to_s[-4, 2], Nomen::Unit[:are].symbol],
+        [[total_area_to_s[0..-5], Onoma::Unit[:hectare].symbol],
+         [total_area_to_s[-4, 2], Onoma::Unit[:are].symbol],
          [total_area_to_s[-2, 2], "ca"]].map(&:join).join(' ')
       },
       short_form_unit: lambda { |measure| "#{measure.value.to_f} #{measure.symbol}" }
