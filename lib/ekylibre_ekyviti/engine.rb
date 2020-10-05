38bd6106531e33d313e85cb2e244151d210a779b
Guillaume <groche@ekylibre.com>
2020-10-05 10:30:12 +0200

    Moved traduction to puggin

--- lib/ekylibre_ekyviti/engine.rb
+++ lib/ekylibre_ekyviti/engine.rb
@@ -3,5 +3,9 @@ module EkylibreEkyviti
     initializer 'ekylibre_ekyviti.assets.precompile' do |app|
       app.config.assets.precompile += %w(ekyviti.scss ekyviti.js)
     end
+
+    initializer :i18n do |app|
+      app.config.i18n.load_path += Dir[EkylibreEkyviti::Engine.root.join('config', 'locales', '**', '*.yml')]
+    end
   end
 end
