3463a64c60aeec55c8ef440d5954093309bae759
Thibaut Gorioux <tgorioux@ekylibre.com>
2020-11-23 16:04:04 +0100

    undo

--- lib/ekylibre_ekyviti/plugin.rb
+++ lib/ekylibre_ekyviti/plugin.rb
@@ -1,7 +1,7 @@
 module EkylibreEkyviti
   class Plugin
     def themes
-      %w[bordeaux cognac].map { |name| Ekylibre::Plugin::Theme.new(name, "#{EkylibreEkyviti::Engine.root.join('app', 'assets', 'stylesheets', 'themes') + '/' + name}/all.css")}
+      %w[bordeaux cognac]
     end
   end
 end
