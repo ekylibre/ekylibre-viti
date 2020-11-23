aea8d874295ded0a3e0f443c0be4ef1055cb7ca2
Thibaut Gorioux <tgorioux@ekylibre.com>
2020-11-23 15:08:28 +0100

    Add themes

--- lib/ekylibre_ekyviti/plugin.rb
+++ lib/ekylibre_ekyviti/plugin.rb
@@ -1,7 +1,7 @@
 module EkylibreEkyviti
   class Plugin
     def themes
-      %w[bordeaux cognac]
+      %w[bordeaux cognac].map { |name| Ekylibre::Plugin::Theme.new(name, "#{EkylibreEkyviti::Engine.root.join('app', 'assets', 'stylesheets', 'themes') + '/' + name}/all.css")}
     end
   end
 end
