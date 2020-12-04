c1d4dd5b8a1a80a2917d99cabc91e0f37e7dc4e3
Thibaut Gorioux <tgorioux@ekylibre.com>
2020-12-04 16:41:46 +0100

    Prefix initializer name

--- lib/ekylibre_ekyviti/engine.rb
+++ lib/ekylibre_ekyviti/engine.rb
@@ -4,7 +4,7 @@ module EkylibreEkyviti
       ::Backend::BaseController.prepend_view_path EkylibreEkyviti::Engine.root.join('app/views')
     end
 
-    initializer :themes do |app|
+    initializer :ekylibre_ekyviti_themes do |app|
       app.config.themes += %w[bordeaux cognac]
     end
 
@@ -12,30 +12,30 @@ module EkylibreEkyviti
       app.config.assets.precompile += %w(ekyviti.scss ekyviti.js themes/bordeaux/all.css themes/cognac/all.css *.svg)
     end
 
-    initializer :extend_navigation do |_app|
+    initializer :ekylibre_ekyviti_extend_with_ekyviti_navigation do |_app|
       EkylibreEkyviti::ExtNavigation.add_navigation_xml_to_existing_tree
     end
 
-    initializer :extend_lexicon do |_app|
+    initializer :ekylibre_ekyviti_extend_lexicon do |_app|
       ::Lexicon.include EkylibreEkyviti::Lexicon
     end
 
-    initializer :i18n do |app|
+    initializer :ekylibre_ekyviti_i18n do |app|
       app.config.i18n.load_path += Dir[EkylibreEkyviti::Engine.root.join('config', 'locales', '**', '*.yml')]
     end
 
-    initializer :extend_controllers do |_app|
+    initializer :ekylibre_ekyviti_extend_controllers do |_app|
       ::Backend::ActivitiesController.include Backend::ActivitiesControllerExt
     end
 
-    initializer :restfully_manageable do |app|
+    initializer :ekylibre_ekyviti_restfully_manageable do |app|
       app.config.x.restfully_manageable.view_paths << EkylibreEkyviti::Engine.root.join('app', 'views')
     end
 
-    initializer :extend_measure do |_app|
+    initializer :ekylibre_ekyviti_extend_measure do |_app|
       ::Measure.prepend EkylibreEkyviti::MeasureExt
     end
-    initializer :beehive do |app|
+    initializer :ekylibre_ekyviti_beehive do |app|
       app.config.x.beehive.cell_controller_types << :weather_vine_spraying_map
     end
   end
